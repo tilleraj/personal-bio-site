f2520d76c2cf26815752459ef601b6e1bba2054c
Andrew Tiller <tiller.andrew@gmail.com>
2019-06-27 14:55:01 -0500

    added theme to directory

--- main.js
+++ main.js
@@ -1,13 +1,3 @@
-// {
-//     title: "Cool Project", 
-//     screenshot: "https://via.placeholder.com/250x250", 
-//     description: "This is the best project", // A good project description includes 'the what', 'the why', and 'the how'.
-//     technologiesUsed: "HTML, CSS, Vanilla JavaScript, Version Control with Github",
-//     available: true,
-//     url: "https://github.com/nss-evening-cohort-8/js-part-deux", // Towards the latter part of the class, you will learn how to host your projects and people will be able to view them live. Cool, right? Welp, until then, just use your GitHub link in this spot as well.
-//     githubUrl: "https://github.com/nss-evening-cohort-8/js-part-deux"
-//   }
-
 const printToDom = (divId, textToPrint) => {
     const selectedDiv = document.getElementById(divId);
     selectedDiv.innerHTML = textToPrint;
